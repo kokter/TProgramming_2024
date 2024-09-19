e1bd1675c5558f877b44126d98511edf12a1f079
J0es1ick <mrjoeslick@gmail.com>
2024-09-19 22:43:32 +0300

    lab 3 ts

--- rpgsaga/saga/src/index.ts
+++ rpgsaga/saga/src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello world');
+function result(a, x) {
+  const resultFunc = Math.pow(a, Math.pow(x, 2) - 1) - Math.log10(Math.pow(x, 2) - 1) + Math.cbrt(Math.pow(x, 2) - 1);
+  return resultFunc;
+}
+
+function taskA(a, xBegin, xEnd, xDelta) {
+  const y = [];
+  for (let x = xBegin; x <= xEnd; x += xDelta) {
+    y.push(result(a, x));
+  }
+  return y;
+}
+
+function taskB(a, values) {
+  const y = [];
+  for (const x of values) {
+    y.push(result(a, x));
+  }
+  return y;
+}
+
+function output(nameOfTask, results) {
+  console.log(`Solutions to task ${nameOfTask}:`);
+  for (const res of results) {
+    console.log(res);
+  }
+}
+
+const taskAResult = taskA(1.6, 1.2, 3.7, 0.5);
+output('A', taskAResult);
+
+const values = [1.28, 1.36, 2.47, 3.68, 4.56];
+const taskBResult = taskB(1.6, values);
+output('B', taskBResult);
