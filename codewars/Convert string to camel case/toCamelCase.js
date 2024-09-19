54b81aa6b80bdde324105cbbf2fbe72212e094e1
J0es1ick <mrjoeslick@gmail.com>
2024-09-19 23:31:00 +0300

    toCamelCase fix

--- codewars/Convert string to camel case/toCamelCase.js
+++ codewars/Convert string to camel case/toCamelCase.js
@@ -5,11 +5,9 @@ function toCamelCase(str) {
   const words = str.split(/[-_]/);
 
   const upperCaseWords = words.map((word, index) => {
-    if (index === 0) {
-      return word.charAt(0) + word.slice(1).toLowerCase();
-    } else {
-      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    }
+    return index === 0
+      ? word.charAt(0) + word.slice(1).toLowerCase()
+      : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   });
 
   return upperCaseWords.join("");
