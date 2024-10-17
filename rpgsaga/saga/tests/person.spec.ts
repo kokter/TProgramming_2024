74fd9397683921f6c395fc5efcc84c62ff98a7b2
J0es1ick <mrjoeslick@gmail.com>
2024-10-17 13:24:00 +0300

    tests fix

--- rpgsaga/saga/tests/person.spec.ts
+++ rpgsaga/saga/tests/person.spec.ts
@@ -3,27 +3,27 @@ import { Person } from '../src/person';
 describe('Person class methods tests', () => {
   it('Constructor test', () => {
     let newPerson = new Person('TestPerson', 20, 'Male');
-    expect(newPerson.age == 20);
-    expect(newPerson.name == 'TestPerson');
-    expect(newPerson.sex == 'Male');
+    expect(newPerson.age).toEqual(20);
+    expect(newPerson.name).toBe('TestPerson');
+    expect(newPerson.sex).toBe('male');
   });
   describe('Get methods tests', () => {
     let newPerson = new Person('TestPerson', 20, 'Male');
     it('Age get test', () => {
-      expect(newPerson.age == 20);
+      expect(newPerson.age).toEqual(20);
     });
     it('Name get test', () => {
-      expect(newPerson.name == 'TestPerson');
+      expect(newPerson.name).toBe('TestPerson');
     });
     it('Sex get test', () => {
-      expect(newPerson.sex == 'Male');
+      expect(newPerson.sex).toBe('male');
     });
   });
   describe('Set methods tests', () => {
     let newPerson = new Person('TestPerson', 20, 'Male');
     it('Age basic test', () => {
       newPerson.age = 35;
-      expect(newPerson.age == 35);
+      expect(newPerson.age).toEqual(35);
     });
     it('Age negative test', () => {
       expect(() => {
@@ -32,11 +32,11 @@ describe('Person class methods tests', () => {
     });
     it('Name test', () => {
       newPerson.name = 'John';
-      expect(newPerson.name == 'John');
+      expect(newPerson.name).toBe('John');
     });
     it('Sex basic test', () => {
       newPerson.sex = 'Female';
-      expect(newPerson.sex == 'female');
+      expect(newPerson.sex).toBe('female');
     });
     it('Sex uncorrect test', () => {
       expect(() => {
@@ -47,13 +47,13 @@ describe('Person class methods tests', () => {
   describe('Other methods tests', () => {
     let newPerson = new Person('John', 20, 'Male');
     it('Should return name and sex', () => {
-      expect(newPerson.toString() == `${newPerson.name} ${newPerson.sex}`);
+      expect(newPerson.toString()).toEqual(`${newPerson.name} ${newPerson.sex}`);
     });
     it('Should return age as number', () => {
-      expect(newPerson.toNumber() == newPerson.age);
+      expect(newPerson.toNumber()).toEqual(newPerson.age);
     });
     it('Should return all properties', () => {
-      expect(newPerson.print() == `first name: ${newPerson.name}\nsex: ${newPerson.sex}\nage: ${newPerson.age}`);
+      expect(newPerson.print()).toEqual(`first name: ${newPerson.name}\nsex: ${newPerson.sex}\nage: ${newPerson.age}`);
     });
   });
 });
